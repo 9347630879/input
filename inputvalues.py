df4c6233847ffb3540aa2a32a2743e98819a0856
9347630879 <145848207+9347630879@users.noreply.github.com>
2023-10-05 13:34:57 +0530

    Add files via upload

---
+++ inputvalues.py
@@ -0,0 +1,177 @@
+#mathematical operations
+
+firstName = input("firstName:")
+lastName = input("lastName:")
+fullName =  firstName + " " + lastName
+print( fullName . title() )
+
+ #(-)
+
+ #subraction
+a = input( " enter a value:")
+b = input( " enter b value:")
+print ( int(a) - int(b))
+
+#addition
+a = input( " enter a value:")
+b = input( " enter b value:")
+print ( int(a) + int(b))
+
+#division
+a = input( " enter a value:")
+b = input( " enter b value:")
+print ( int(a) / int(b))
+
+#mulitiplication
+a = input( " enter a value:")
+b = input( " enter b value:")
+print ( int(a) * int(b))
+
+#module
+
+a = input( " enter a value:")
+b = input( " enter b value:")
+print ( int(a) % int(b))
+
+#floar division
+a = input( " enter a value:")
+b = input( " enter b value:")
+print ( int(a) // int(b))
+
+#equal
+
+a = input( " enter a value:")
+b = input( " enter b value:")
+print ( int(a) == int(b))
+
+#< =
+
+a = input( " enter a value:")
+b = input( " enter b value:")
+print ( int(a) <= int(b))
+
+#>=
+
+a = input( " enter a value:")
+b = input( " enter b value:")
+print ( int(a) >= int(b))
+
+#and
+a = input( " enter a value:")
+b = input( " enter b value:")
+print ( int(a) and int(b))
+
+#or
+
+a = input( " enter a value:")
+b = input( " enter b value:")
+print ( int(a) | int(b))
+
+#xor
+
+a = input( " enter a value:")
+b = input( " enter b value:")
+print ( int(a) ^ int(b))
+
+#is
+a = input( " enter a value:")
+b = input( " enter b value:")
+print ( int(a) is int(b))
+
+#isnot
+
+a = input( " enter a value:")
+b = input( " enter b value:")
+print ( int(a) is not int(b))
+
+#strings
+
+
+firstName = input("firstName:")
+lastName = input("lastName:")
+fullName =  firstName + " " + lastName
+print( fullName . title() )
+
+
+firstName = input("firstName:")
+lastName = input("lastName:")
+fullName =  firstName + " " + lastName
+print( fullName . capitalize() )
+
+firstName = input("firstName:")
+lastName = input("lastName:")
+fullName =  firstName + " " + lastName
+print( fullName . swapcase() )
+
+
+firstName = input("firstName:")
+lastName = input("lastName:")
+fullName =  firstName + " " + lastName
+print( fullName . islower() )
+
+firstName = input("firstName:")
+lastName = input("lastName:")
+fullName =  firstName + " " + lastName
+print( fullName . isupper() )
+
+
+firstName = input("firstName:")
+lastName = input("lastName:")
+fullName =  firstName + " " + lastName
+print( fullName . swapcase() )
+
+
+firstName = input("firstName:")
+lastName = input("lastName:")
+fullName =  firstName + " " + lastName
+print( fullName . isidentifier() )
+
+
+firstName = input("firstName:")
+lastName = input("lastName:")
+fullName =  firstName + " " + lastName
+print( fullName . isdecimal() )
+
+
+firstName = input("firstName:")
+lastName = input("lastName:")
+fullName =  firstName + " " + lastName
+print( fullName . isdigit() )
+
+
+firstName = input("firstName:")
+lastName = input("lastName:")
+fullName =  firstName + " " + lastName
+print( fullName . isprintable() )
+
+firstName = input("firstName:")
+lastName = input("lastName:")
+fullName =  firstName + " " + lastName
+print( fullName . isidentifier() )
+
+firstName = input("firstName:")
+lastName = input("lastName:")
+fullName =  firstName + " " + lastName
+print( fullName . isspace() )
+
+firstName = input("firstName:")
+lastName = input("lastName:")
+fullName =  firstName + " " + lastName
+print( fullName . isalnum() )
+
+firstName = input("firstName:")
+lastName = input("lastName:")
+fullName =  firstName + " " + lastName
+print( fullName . isalpha() )
+
+
+a = bin(input( " 5 "))
+print( a.isalpha())
+
+
+a = bin(input( " sai "))
+print( a.isalpha())
+
+
+
+
